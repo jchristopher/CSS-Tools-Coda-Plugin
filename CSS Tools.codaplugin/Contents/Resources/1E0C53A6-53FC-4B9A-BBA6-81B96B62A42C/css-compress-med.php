5217bd7eb75bad01f3b876462eddfc6c114acf70
Jonathan Christopher <jonathan@irontoiron.com>
2011-08-13 09:54:48 -0400

    Initial commit

---
+++ CSS Tools.codaplugin/Contents/Resources/1E0C53A6-53FC-4B9A-BBA6-81B96B62A42C/css-compress-med.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+
+$ext = '';
+$input = '';
+
+$ext = strtolower(strrchr($_ENV['CODA_FILEPATH'], '.'));
+
+if($ext==".css") {
+
+	// We need CSSTidy
+	require_once($_ENV['CODA_BUNDLE_SUPPORT'] . "/class.csstidy.php");
+	require_once($_ENV['CODA_BUNDLE_SUPPORT'] . "/lang.inc.php");
+
+	$fp = fopen("php://stdin", "r");
+	while ( $line = fgets($fp, 1024) )
+		$input .= $line;
+	fclose($fp);
+
+	$css = new csstidy();
+	
+	$css->set_cfg('lowercase_s',true);
+	$css->set_cfg('compress_colors',true);
+	$css->set_cfg('compress_font-weight',true);
+	$css->set_cfg('remove_last_;',true);
+	$css->set_cfg('discard_invalid_properties',true);
+	
+$compress_medium = '<span class="at">|</span> <span class="format">{</span>
+|<span class="selector">|</span><span class="format">{</span>|<span class="property">|</span><span class="value">|</span><span class="format">;</span>|<span class="format">}</span>
+||<span class="format">}
+</span>||</span> <span class="format">{</span>|
+';
+	
+	$css->load_template($compress_medium,false);
+	$result = $css->parse($input);
+	
+	if($result) {
+		echo $css->print->plain();
+	} else {
+		echo "/* CSS Reformatting Error */\n\n";
+		echo $input;
+	}
+
+}
+
+?>
---
+++ CSS Tools.codaplugin/Contents/Resources/A0923DAD-56DD-4791-8100-AB8D0CC2F061/css-reformat-multi-line.php
@@ -0,0 +1,58 @@
+#!/usr/bin/php
+<?php
+
+$ext = '';
+$input = '';
+
+$ext = strtolower(strrchr($_ENV['CODA_FILEPATH'], '.'));
+
+if($ext==".css") {
+
+	// We need CSSTidy
+	require_once($_ENV['CODA_BUNDLE_SUPPORT'] . "/class.csstidy.php");
+	require_once($_ENV['CODA_BUNDLE_SUPPORT'] . "/lang.inc.php");
+
+	$fp = fopen("php://stdin", "r");
+	while ( $line = fgets($fp, 1024) )
+		$input .= $line;
+	fclose($fp);
+
+	$css = new csstidy();
+	
+	$css->set_cfg('lowercase_s',false);
+	$css->set_cfg('compress_colors',false);
+	$css->set_cfg('compress_font-weight',false);
+	$css->set_cfg('remove_bslash',false);
+	$css->set_cfg('preserve_css',true);
+	$css->set_cfg('sort_selectors',false);
+	$css->set_cfg('sort_properties',false);
+	$css->set_cfg('remove_last_;',false);
+	$css->set_cfg('discard_invalid_properties',false);
+	$css->set_cfg('timestamp',false);
+
+$template_multi_line = '<span class="at">|</span> <span class="format"></span>
+|<span class="selector">|</span> <span class="format">{</span>
+|	<span class="property">|</span><span class="value">|</span><span class="format">;</span>
+|<span class="format">}</span>|
+
+|
+<span class="format">}</span>
+
+|	|</span>
+	<span class="format"></span>
+|
+';
+	
+	$css->load_template($template_multi_line,false);
+	$result = $css->parse($input);
+	
+	if($result) {
+		echo $css->print->plain();
+	} else {
+		echo "/* CSS Reformatting Error */\n\n";
+		echo $input;
+	}
+
+}
+
+?>
